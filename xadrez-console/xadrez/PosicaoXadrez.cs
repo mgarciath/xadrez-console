eb754b040bf1e33b4abeabf97b93863ffa37ab54
Matheus Garcia <mgarciapxt@gmail.com>
2025-09-24 14:10:28 -0300

    Criada a classe PosicaoXadrez.

---
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace xadrez {
+    class PosicaoXadrez {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+        
+        public PosicaoXadrez(char coluna, int linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+    }
+}
